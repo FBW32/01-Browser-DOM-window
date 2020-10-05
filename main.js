4c27e9ad17969e1fb7c90c384b143d987d124103
Misbah-D <misbah.deeb@gmail.com>
2020-10-05 14:34:45 +0200

    done

---
+++ main.js
@@ -0,0 +1,19 @@
+
+let answer = prompt("Guess a number from 1-10")
+let num = 0
+a= parseInt(answer);
+b = parseInt(num);
+
+for ( i = 0; i < 3; i++) {
+    if (a===b) { prompt ("correct the number you added is correct")
+    
+    } else {
+        prompt("You have three attempts to guess the correct number!")
+        prompt("not correct")
+        prompt ("the number is 0")
+    
+}
+    
+}
+
+
