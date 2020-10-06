4e0ccb83773ec81b43c2e1f2f573b2cdfc8bab5b
Misbah-D <misbah.deeb@gmail.com>
2020-10-06 09:32:27 +0200

    edited

--- main.js
+++ main.js
@@ -4,13 +4,15 @@ let num = 0
 a= parseInt(answer);
 b = parseInt(num);
 
-for ( i = 0; i < 3; i++) {
-    if (a===b) { prompt ("correct the number you added is correct")
+for ( let i = 0; i < 3; i++) {
+    if (a === b) {
+        alert("correct the number you added is correct"); break;
     
     } else {
         prompt("You have three attempts to guess the correct number!")
         prompt("not correct")
-        prompt ("the number is 0")
+        alert("the number is 0")
+        break;
     
 }
     
